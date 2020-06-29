05174455921db2511c680ca90da2191fc366cb07
CamMcP <52165008+CamMcP@users.noreply.github.com>
2020-06-29 00:28:20 +0000

    Add files via upload

--- src/_01_nasty_surprise/Nasty.java
+++ src/_01_nasty_surprise/Nasty.java
@@ -1,7 +1,36 @@
 package _01_nasty_surprise;
 
+import javax.swing.JButton;
+
+import javax.swing.JFrame;
+
+import javax.swing.JLabel;
+
+import javax.swing.JPanel;
+
 public class Nasty {
-public static void main(String[] args) {
 	
+public static void main(String[] args) {	
+JButton A = new JButton();
+JButton B = new JButton();
+
+JFrame AA = new JFrame();
+JPanel Hold = new JPanel();
+
+JLabel a = new JLabel();
+JLabel b = new JLabel();
+
+a.setText("Trick");
+b.setText("Treat");
+
+A.add(a);
+B.add(b);
+
+Hold.add(A);
+Hold.add(B);
+AA.add(Hold);
+AA.setVisible(true);
+AA.setSize(100, 100);
 }
+
 }
