83991f9df9d32f01dc1f26e5200247fca8313d2c
root <root@ca92a6505a56>
2020-10-05 00:25:20 +0000

    aaaaa

--- src/_11_whack_a_mole/Wack.java
+++ src/_11_whack_a_mole/Wack.java
@@ -2,17 +2,22 @@ package _11_whack_a_mole;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 import java.util.RandomAccess;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Wack implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	Date timeAtStart = new Date();
+	int hits = 0;
+	int fails = 0;
 	public void run() {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
 		Random rand = new Random();
 		int mole = rand.nextInt(19);
 		for (int i = 0; i < 18; i++) {
@@ -50,15 +55,56 @@ public class Wack implements ActionListener {
             }
         }
     }
+	
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date();
+	    JOptionPane.showMessageDialog(null, "Your whack rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	                  + " moles per second.");
+	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		JButton buttonClicked = (JButton) arg0.getSource();
 		if(buttonClicked.getText().equals("mole!")){
-		
-	}
+			System.out.println("Ouch!");
+			frame.dispose();
+			frame.remove(panel);
+			panel=new JPanel();
+			frame.add(panel);
+			Random rand = new Random();
+			int mole = rand.nextInt(19);
+			for (int i = 0; i < 18; i++) {
+				if (i == mole) {
+					JButton button = new JButton("mole!");
+					button.setSize(100, 100);
+					button.addActionListener(this);
+					panel.add(button);
+				}
+				else {
+					JButton button = new JButton("     ");
+					button.setSize(50, 50);
+					button.addActionListener(this);
+					panel.add(button);			
+				}
+			}
+			frame.setVisible(true);
+			hits = hits + 1;
+			if (hits == 10) {
+				endGame(timeAtStart, 10);
+			}
+			}
+	
 		else {
 			System.out.println("You missed!");
+			fails = fails + 1;
+			if (fails == 5) {
+				System.out.println("You have failed. Better luck next time!");
+				endGame(timeAtStart, hits);
+			} 
+			else {
+
+			}
 }
 	}
 }
